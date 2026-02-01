3631108d042f9e8dacb4a646f855ff68764d167c
odegaardquantium-code <odegaardquantium@gmail.com>
2026-02-01 20:59:47 +0100

    Delete src directory

--- src/cron/types.d.ts
+++
@@ -1,4 +0,0 @@
-export interface TonAddressData {
-  tonWalletAddress: string;
-  jettonWalletAddress: string;
-}
--- src/utils/runtime.ts
+++
@@ -1,5 +0,0 @@
-export async function waitTONRPSDelay() {
-  await new Promise((resolve) =>
-    setTimeout(resolve, parseInt(process.env.RPS_DELAY ?? "1000")),
-  );
-}
